0b253650cebe3a500cfd84ea282a7288c9217860
Nur Farahana <95182496+Farahana16@users.noreply.github.com>
2021-12-12 00:05:51 +0800

    Add files via upload

--- tiedye.sql
+++ tiedye.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 11, 2021 at 02:28 PM
+-- Generation Time: Dec 11, 2021 at 05:05 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.0
 
@@ -44,8 +44,30 @@ CREATE TABLE `customers` (
 CREATE TABLE `product` (
   `productID` int(11) NOT NULL,
   `productName` varchar(25) NOT NULL,
-  `productPrice` double NOT NULL
+  `productPrice` float NOT NULL,
+  `productImages` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`productID`, `productName`, `productPrice`, `productImages`) VALUES
+(1, 'Friends TieDye', 15, '1p.png'),
+(2, 'Rainbow TieDye', 15, '2p.png'),
+(3, 'Crop Top TieDye', 10, '3p.png'),
+(4, 'Bear TieDye', 15, '4p.png'),
+(5, 'Blue TieDye', 10, '5p.png'),
+(6, 'Light Blue TieDye', 5, '6p.png'),
+(7, 'Rainbow 2 TieDye', 10, '7p.png'),
+(8, 'Black Rainbow TieDye', 10, '8p.png'),
+(9, 'Black TieDye', 10, '9p.png'),
+(10, 'Orange TieDye', 10, '10p.png'),
+(11, 'Cloud TieDye', 10, '11p.png'),
+(12, 'Two Tone TieDye', 10, '12p.png'),
+(13, 'Rainbow 3 TieDye', 15, '13p.png'),
+(14, 'Rainbow 4 TieDye', 15, '14p.png'),
+(15, 'Rainbow 5', 15, '15p.png');
 
 --
 -- Indexes for dumped tables
@@ -77,7 +99,7 @@ ALTER TABLE `customers`
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `productID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `productID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
